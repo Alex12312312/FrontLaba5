2ccadf1296b1aef848d26c01132286d1f221edb4
Alex <sasha_novosel@mail.ru>
2023-11-26 20:53:06 +0300

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	deleted:    public/favicon.ico 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/manifest.json 	deleted:    public/robots.txt 	deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js
     Untracked files:
    	src/Components/
    	src/Util/
    	src/scripts/

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import styles from './index.css'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Authpage from "./Components/AuthPage";
+import Link from "react-router-dom"
+import SiteBody from "./Components/MainBody"
+import SiteHead from "./Components/SiteHead"
+import RegPage from "./Components/RegPage"
+import Profile from "./Components/Profile"
+import { AuthContext } from "./Util/AuthContext";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App(){
+const [isAuth, setAuthStatus] = useState(false)
+  return(
+  <AuthContext.Provider value={{
+      isAuth,
+      setAuthStatus
+  }}>
+  <BrowserRouter>
+    <SiteHead/>
+    <Routes>
+    <Route exact path="/" element={<SiteBody/>}/>
+    <Route path="/login" element={<Authpage/>}/>
+    <Route path="/reg" element={<RegPage/>}/>
+    <Route path="/lk" element={<Profile/>}/>
+    <Route path="*" element={<Navigate to={{pathname:"/"}}/>}/>
+    </Routes>
+      </BrowserRouter>
+      </AuthContext.Provider>)
+        }
 
 export default App;

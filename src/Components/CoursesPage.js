355ec90201733a13ff8001905925d812ada6d3f2
Alex <sasha_novosel@mail.ru>
2023-12-08 20:47:41 +0300

    FixCourses

--- src/Components/CoursesPage.js
+++ src/Components/CoursesPage.js
@@ -2,26 +2,33 @@ import axios from 'axios'
 import styles from '../index.css'
 import React, { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../Util/AuthContext";
+import { useNavigate } from "react-router-dom";
 function CoursesPage(){
+    const navigate = useNavigate()
     const {isAuth, setAuthStatus} = useContext(AuthContext)
     const [items, setItems] = useState([])
     const [userCourses, setUserCourses] = useState([])
+
     const getUserCourses = () => {
         axios({
             method: 'get',
             url: 'http://localhost:8080/course/user',
-            headers: {'Authorization': Number(localStorage.session_id)},
-            params: {user_id: localStorage.user_id}
-        })
+            headers: {Authorization: Number(localStorage.session_id)},
+            params: {user_id: Number(localStorage.user_id)}
+        }).then((response) => {
+            const data = response.data.map(elem=>{return [elem.name,`data:image/png;base64,${elem.image}`, elem.description, elem.id]});
+            setUserCourses(data);}
+        )
     };
+    
     useEffect(() => {
         const fetchData = () => {
-            getUserCourses();
             axios.get('http://localhost:8080/course/all')
             .then((response) => {
             const data = response.data.map(elem=>{return [elem.name,`data:image/png;base64,${elem.image}`, elem.description, elem.id]});
             setItems(data);})
-        };
+        }
+    getUserCourses()
     fetchData()
     }, [])
     const addCourse = (courseIndex) => {
@@ -33,14 +40,25 @@ function CoursesPage(){
             user_id: localStorage.user_id}
         })
         .then((response) => {
-            
+        })
+        .catch((error) => {
+            if(error.response.status == 401){
+                localStorage.user_id = -1
+                localStorage.session_id = -1
+                setAuthStatus(false)
+                navigate("/")
+            } 
+            else if(error.response.status == 400){
+                alert("Вы уже записаны на этот курс")
+            }
         })
     }
     return(<div className='CoursesPage'>
          {items.map((text, index) => (
     <div key={"item"+index} className="CoursePageItem"  title={text[2]}><div key={'text' + index}>{text[0]}</div>
     <img className="CourseItemIMG" key={"image" + index} src={text[1]}></img>
-    {(isAuth && !(text in userCourses))?<button onClick={(e) => {addCourse(text[3])}}>Записаться</button>:<></>}</div>
+    {(isAuth)?<button onClick={(e) => {addCourse(text[3])}} disabled={text[4]}>
+        Записаться</button>:<></>}</div>
     ))}
     </div>)
 }

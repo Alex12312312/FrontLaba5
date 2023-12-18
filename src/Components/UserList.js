6ced725c7db5346fd1c12657862b9dd9afe2150f
Alex <sasha_novosel@mail.ru>
2023-12-18 22:30:57 +0300

    CheckUserDelete

--- src/Components/UserList.js
+++ src/Components/UserList.js
@@ -1,26 +1,38 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
+import { Link } from "react-router-dom";
 function UserList(){
     const [users, setUsers] = useState([])
     const getUsers = () => {
         axios.get("http://localhost:8080/user/all", {headers:{
             Authorization: localStorage.session_id
         }}).then((response)=>{
-            console.log(response.data)
             setUsers(response.data)
-            console.log(users)
         })
     }
     useEffect(()=>{
         getUsers()
     }, [])
+    const deleteUser = (event, userId) => {
+        event.preventDefault()
+        axios({
+            method: 'post',
+            url: 'http://localhost:8080/course/user/add',
+            headers: {'Authorization': Number(localStorage.session_id)},
+            params: {user_id: userId}
+        })
+    }
     return(<div className="sitePage" id="userListPage">
         <div id="userTable">
-            {users.map((data, index)=>(<div className="userTableItem" id={"item" + data.id}>
-                <div id={"RS" + data.id} className="tableRightSide">
-                    <img src={`data:image/${data.avatar[0]};base64,${data.avatar[1]}`}></img>
+            {users.map((data, index)=>(<div className="userTableItem" key={"item" + data.id}>
+                <div key={"RS" + data.id} className="tableRightSide">
+                    <img className="NewNewsImageItem" src={`data:image/${data.avatar[0]};base64,${data.avatar[1]}`}></img>
+                    <div>{data.login}</div>
                 </div>
-                <div id={"LS" + data.id} className="tableLeftSide"></div>
+                <div key={"LS" + data.id} className="tableLeftSide">
+                    <div  className="deleteUserButton" onClick={(e) => {deleteUser(e, data.id)}}>Удалить</div>
+                    <Link className="deleteUserButton">Редактировать</Link>
+                </div>
             </div>))}
         </div>
     </div>)
